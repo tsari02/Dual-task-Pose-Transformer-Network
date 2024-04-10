8afc3ca26fc2f4b940cac6cc672891aa5bf2b98f
tsari02 <sariteshtiwari@gmail.com>
2024-04-11 01:26:10 +0530

    correct generate dataset

--- data/generate_fashion_datasets.py
+++ data/generate_fashion_datasets.py
@@ -44,19 +44,29 @@ def make_dataset(dir):
 
 	for root, _, fnames in sorted(os.walk(dir)):
 		for fname in fnames:
+			# print(fname)
 			if is_image_file(fname):
 				path = os.path.join(root, fname)
+				# print(path)
 				path_names = path.split('/') 
+				
+				del path_names[2]
+				del path_names[0]
+				# print(path_names)
 				# path_names[2] = path_names[2].replace('_', '')
 				path_names[3] = path_names[3].replace('_', '')
 				path_names[4] = path_names[4].split('_')[0] + "_" + "".join(path_names[4].split('_')[1:])
 				path_names = "".join(path_names)
+				# print(root)
 				# new_path = os.path.join(root, path_names)
+				# print(path_names)
 				img = Image.open(path)
 				imgcrop = img.crop((40, 0, 216, 256))
-				if new_path in train_images:
+				if path_names in train_images:
 					imgcrop.save(os.path.join(train_root, path_names))
-				elif new_path in test_images:
+				elif path_names in test_images:
 					imgcrop.save(os.path.join(test_root, path_names))
 
+
+os.chdir('/content/Dual-task-Pose-Transformer-Network/dataset')
 make_dataset('./fashion')
